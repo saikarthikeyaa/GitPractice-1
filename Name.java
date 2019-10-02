ecb0a11ce5a7edf73c7b972f486f7a8622bd4c11
PCSE User <pcse_user@pcs.cnu.edu>
2019-10-02 16:47:55 -0400

    Fixed Name.java

--- Name.java
+++ Name.java
@@ -31,34 +31,34 @@ public class Name {
     }
 
     public String getTitle() {
-        /* return Name's title */
+        return title;
     }
 
     public String getFirst() {
-        /* return Name's first name */
+        return first;
     }
 
     public String getMiddle() {
-        /* return Name's middle name */
+        return middle;
     }
 
     public String getSurname() {
-        /* return Name's last name */
+        return surname;
     }
 
     public String getSurnameParent1() {
-        /* return parent1's last name */
+        return surnameParent1;
     }
 
     public String getSurnameParent2() {
-        /* return parent2's last name */
+        return surnameParent2;
     }
 
     public String getSuffixProfessional() {
-    	/* return Name's professional suffix */
+    	return suffixProfessional;
     }
 
     public String getSuffixGenerational() {
-        /* return Name's generational suffix */
+    	return suffixGenerational;
     }
 }
